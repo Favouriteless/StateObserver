8299d3298ea3b44cb590da5673e3cc5e36e73e48
Favouriteless <62647254+Favouriteless@users.noreply.github.com>
2025-05-12 15:15:12 +0100

    Performance change

--- common/src/main/java/net/favouriteless/stateobserver/StateObserverManagerImpl.java
+++ common/src/main/java/net/favouriteless/stateobserver/StateObserverManagerImpl.java
@@ -3,33 +3,37 @@ package net.favouriteless.stateobserver;
 import net.favouriteless.stateobserver.api.StateObserver;
 import net.favouriteless.stateobserver.api.StateObserverManager;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.SectionPos;
+import net.minecraft.world.level.ChunkPos;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Stream;
 
 public class StateObserverManagerImpl implements StateObserverManager {
 
 	public static final StateObserverManagerImpl INSTANCE = new StateObserverManagerImpl();
 
-	private final List<StateObserver> observers = new ArrayList<>();
+	private final Map<ChunkPos, Set<StateObserver>> observers = new HashMap<>();
+
 
 	public <T extends StateObserver> T addObserver(@NotNull T observer) {
-		observers.add(observer);
+		getObservedChunks(observer).forEach(c -> observers.computeIfAbsent(c, k -> new HashSet<>()).add(observer));
 		observer.onInit();
 		return observer;
 	}
 
 	public void removeObserver(@NotNull StateObserver observer) {
 		observer.onRemove();
-		observers.remove(observer);
+		getObservedChunks(observer).forEach(c -> Optional.ofNullable(observers.get(c)).ifPresent(s -> s.remove(observer)));
 	}
 
 	@SuppressWarnings("unchecked") // This is an "unchecked" cast-- class is checked instead.
 	public <T extends StateObserver> T getObserver(@NotNull Level level, @NotNull BlockPos pos, @NotNull Class<T> clazz) {
-		for(StateObserver observer : observers) {
+		ChunkPos chunkPos = new ChunkPos(SectionPos.blockToSectionCoord(pos.getX()), SectionPos.blockToSectionCoord(pos.getZ()));
+		for(StateObserver observer : observers.get(chunkPos)) {
 			if(observer.getClass() == clazz) {
 				if(observer.getLevel() == level && observer.getPos().equals(pos))
 					return (T)observer;
@@ -39,11 +43,26 @@ public class StateObserverManagerImpl implements StateObserverManager {
 	}
 
 	public void notifyChange(Level level, BlockPos pos, BlockState oldState, BlockState newState) {
-		for(StateObserver observer : observers) {
-			if(level == observer.getLevel() && observer.isWithinBounds(pos)) {
+		ChunkPos chunkPos = new ChunkPos(SectionPos.blockToSectionCoord(pos.getX()), SectionPos.blockToSectionCoord(pos.getZ()));
+		for(StateObserver observer : observers.get(chunkPos)) {
+			if(level == observer.getLevel() && observer.isWithinBounds(pos))
 				observer.getChangeSet().change(pos, oldState, newState);
-			}
 		}
+	}
+
+	protected Stream<ChunkPos> getObservedChunks(StateObserver observer) {
+		BlockPos center = observer.getPos();
+
+		return ChunkPos.rangeClosed(
+				new ChunkPos(
+						SectionPos.blockToSectionCoord(center.getX() - observer.getRadiusX()),
+						SectionPos.blockToSectionCoord(center.getZ() - observer.getRadiusZ())
+				),
+				new ChunkPos(
+						SectionPos.blockToSectionCoord(center.getX() + observer.getRadiusX()),
+						SectionPos.blockToSectionCoord(center.getZ() + observer.getRadiusZ())
+				)
+		);
 	}
 
 	public void reset() {
